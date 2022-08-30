00d0fab88589d90b4d4f74b3509f9c352b588c86
Seweryn Tałaj <seweryntalaj@Seweryns-MacBook-Pro.local>
2022-08-30 15:50:12 +0200

    Create main class

---
+++ src/main/java/com/github/Seweryn91/WhatTheFridge/WhatTheFridgeApplication.java
@@ -0,0 +1,13 @@
+package com.github.Seweryn91.WhatTheFridge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhatTheFridgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhatTheFridgeApplication.class, args);
+	}
+
+}
