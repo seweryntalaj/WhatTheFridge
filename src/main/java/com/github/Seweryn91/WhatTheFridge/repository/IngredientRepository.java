1e45c6da5bb9c9053f0fb15140764c0fde7eb9ff
Seweryn Talaj <s.talaj@gmail.com>
2022-08-30 17:14:47 +0200

    Update IngredientRepository interface

--- src/main/java/com/github/Seweryn91/WhatTheFridge/repository/IngredientRepository.java
+++ src/main/java/com/github/Seweryn91/WhatTheFridge/repository/IngredientRepository.java
@@ -1,4 +1,9 @@
 package com.github.Seweryn91.WhatTheFridge.repository;
 
-public interface IngredientRepository {
+import com.github.Seweryn91.WhatTheFridge.model.Ingredient;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IngredientRepository extends JpaRepository<Ingredient, Long> {
 }
