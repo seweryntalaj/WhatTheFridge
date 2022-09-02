7320a27998f72d53211deddb9d7f7e60e6c5c411
Seweryn Talaj <s.talaj@gmail.com>
2022-09-02 16:03:19 +0200

    Implement methods

--- src/main/java/com/github/Seweryn91/WhatTheFridge/service/IngredientServiceImpl.java
+++ src/main/java/com/github/Seweryn91/WhatTheFridge/service/IngredientServiceImpl.java
@@ -1,4 +1,48 @@
 package com.github.Seweryn91.WhatTheFridge.service;
 
-public class IngredientServiceImpl {
+import com.github.Seweryn91.WhatTheFridge.model.Ingredient;
+import com.github.Seweryn91.WhatTheFridge.repository.IngredientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+public class IngredientServiceImpl implements IngredientService {
+
+    @Autowired
+    private IngredientRepository ingredientRepository;
+
+    public void setIngredientRepository(IngredientRepository ingredientRepository) {
+        this.ingredientRepository = ingredientRepository;
+    }
+
+    @Override
+    public Ingredient saveIngredient(Ingredient ingredient) {
+        return ingredientRepository.save(ingredient);
+    }
+
+    @Override
+    public List<Ingredient> getAllIngredients() {
+        return ingredientRepository.findAll();
+    }
+
+
+    @Override
+    public Ingredient getIngredientById(long id) {
+        return ingredientRepository.findById(id).get();
+    }
+
+    @Override
+    public void deleteIngredientById(long id) {
+        ingredientRepository.delete(ingredientRepository.findById(id).get());
+    }
+
+    @Override
+    public Ingredient getIngredientByName(String name) {
+        return ingredientRepository.findByName(name);
+    }
+
+    @Override
+    public boolean isFound(long id) {
+        return ingredientRepository.findById(id).isPresent();
+    }
 }
