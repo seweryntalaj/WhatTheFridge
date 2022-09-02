01fda4769a423fcc797f3b72c174af0a648e660a
Seweryn Talaj <s.talaj@gmail.com>
2022-09-02 15:50:20 +0200

    Add interface methods

--- src/main/java/com/github/Seweryn91/WhatTheFridge/service/DishService.java
+++ src/main/java/com/github/Seweryn91/WhatTheFridge/service/DishService.java
@@ -1,4 +1,29 @@
 package com.github.Seweryn91.WhatTheFridge.service;
 
+import com.github.Seweryn91.WhatTheFridge.model.Dish;
+import com.github.Seweryn91.WhatTheFridge.model.Ingredient;
+
+import java.util.List;
+import java.util.Set;
+
 public interface DishService {
+
+    Dish getDishById(long id);
+
+    List<Dish> getDishList();
+
+    Set<Ingredient> getIngredients(long dishId);
+
+    void deleteDishById(Long dishId);
+
+    Set<Dish> findByIngredientNamesIn(Set<String> ingredientsNames);
+
+    Dish saveOrUpdate(Dish dish);
+
+    Dish findByName(String name);
+
+    Set<Dish> findSweetByIngredientNamesIn(Set<String> ingredientsNames);
+
+    Set<Dish> findSavoryByIngredientNamesIn(Set<String> ingredientNames);
+
 }
