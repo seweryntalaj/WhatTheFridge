90c473ceed96a8e78d5556ca742c1745abe3309b
Seweryn Talaj <s.talaj@gmail.com>
2022-09-02 15:47:46 +0200

    Add interface methods

--- src/main/java/com/github/Seweryn91/WhatTheFridge/service/IngredientService.java
+++ src/main/java/com/github/Seweryn91/WhatTheFridge/service/IngredientService.java
@@ -1,4 +1,21 @@
 package com.github.Seweryn91.WhatTheFridge.service;
 
+import com.github.Seweryn91.WhatTheFridge.model.Ingredient;
+
+import java.util.List;
+
 public interface IngredientService {
+
+    Ingredient saveIngredient(Ingredient ingredient);
+
+    List<Ingredient> getAllIngredients();
+
+    Ingredient getIngredientById(long id);
+
+    void deleteIngredientById(long id);
+
+    Ingredient getIngredientByName(String name);
+
+    boolean isFound(long id);
+
 }
