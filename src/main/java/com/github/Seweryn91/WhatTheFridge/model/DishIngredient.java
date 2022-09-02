4185d78d6b1273596a6282bbcd6130590c872a40
Seweryn Talaj <s.talaj@gmail.com>
2022-09-02 15:41:54 +0200

    Add methods to class DishIngredient

--- src/main/java/com/github/Seweryn91/WhatTheFridge/model/DishIngredient.java
+++ src/main/java/com/github/Seweryn91/WhatTheFridge/model/DishIngredient.java
@@ -1,2 +1,42 @@
-package com.github.Seweryn91.WhatTheFridge.model;public class DishIngredient {
+package com.github.Seweryn91.WhatTheFridge.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "dish_ingredient")
+public class DishIngredient {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @ManyToOne
+    private Dish dish;
+
+    @ManyToOne
+    private Ingredient ingredient;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Dish getDish() { return dish; }
+
+    public void setDish(Dish dish) {
+        this.dish = dish;
+    }
+
+    public Ingredient getIngredient() {
+        return ingredient;
+    }
+
+    public void setIngredient(Ingredient ingredient) {
+        this.ingredient = ingredient;
+    }
+
 }
