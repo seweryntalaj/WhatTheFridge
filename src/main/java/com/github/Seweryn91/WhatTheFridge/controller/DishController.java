0e671e8c308436df0f4cf37e688705a276c13a99
Seweryn Talaj <s.talaj@gmail.com>
2022-09-13 16:06:56 +0200

    Create method getDish

--- src/main/java/com/github/Seweryn91/WhatTheFridge/controller/DishController.java
+++ src/main/java/com/github/Seweryn91/WhatTheFridge/controller/DishController.java
@@ -1,12 +1,16 @@
 package com.github.Seweryn91.WhatTheFridge.controller;
 
 import com.github.Seweryn91.WhatTheFridge.model.Dish;
+import com.github.Seweryn91.WhatTheFridge.model.Ingredient;
 import com.github.Seweryn91.WhatTheFridge.service.DishService;
 import com.github.Seweryn91.WhatTheFridge.service.IngredientService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
 
 @Controller
 public class DishController {
@@ -23,4 +27,14 @@ public class DishController {
         model.addAttribute("ingredients", ingredientService.getAllIngredients());
         return "newdish";
     }
+
+    @GetMapping("/dish/get/{id}")
+    public String getDish(@PathVariable("id") long id, Model model) {
+        Dish dish = dishService.getDishById(id);
+        model.addAttribute("dish", dish);
+        List<Ingredient> ingredientsSorted = dish.getIngredients().stream().sorted().toList();
+        model.addAttribute("ingredients", ingredientsSorted);
+        return "dish";
+    }
+
 }
