fcb21692d0a47d16ca1a62eb2703f7b0fb6e99d1
Seweryn Talaj <s.talaj@gmail.com>
2022-09-13 16:05:42 +0200

    Create method addNewDish

--- src/main/java/com/github/Seweryn91/WhatTheFridge/controller/DishController.java
+++ src/main/java/com/github/Seweryn91/WhatTheFridge/controller/DishController.java
@@ -1,4 +1,26 @@
 package com.github.Seweryn91.WhatTheFridge.controller;
 
+import com.github.Seweryn91.WhatTheFridge.model.Dish;
+import com.github.Seweryn91.WhatTheFridge.service.DishService;
+import com.github.Seweryn91.WhatTheFridge.service.IngredientService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class DishController {
+
+    @Autowired
+    private DishService dishService;
+
+    @Autowired
+    private IngredientService ingredientService;
+
+    @GetMapping("/dish/new")
+    public String addNewDish(Model model) {
+        model.addAttribute("dish", new Dish());
+        model.addAttribute("ingredients", ingredientService.getAllIngredients());
+        return "newdish";
+    }
 }
