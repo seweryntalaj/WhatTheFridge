5ccb12a684d5e93cc58fc6ff9f01086d84581677
Seweryn Talaj <s.talaj@gmail.com>
2022-09-13 15:55:45 +0200

    Create method createIngredient

--- src/main/java/com/github/Seweryn91/WhatTheFridge/controller/IngredientController.java
+++ src/main/java/com/github/Seweryn91/WhatTheFridge/controller/IngredientController.java
@@ -1,4 +1,22 @@
 package com.github.Seweryn91.WhatTheFridge.controller;
 
+import com.github.Seweryn91.WhatTheFridge.model.Ingredient;
+import com.github.Seweryn91.WhatTheFridge.service.IngredientService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class IngredientController {
+
+    @Autowired
+    IngredientService ingredientService;
+
+    @GetMapping("/ingredient/new")
+    public String createIngredient(Model model) {
+        Ingredient ingredient = new Ingredient();
+        model.addAttribute("ingredient", ingredient);
+        return "newingredient";
+    }
 }
