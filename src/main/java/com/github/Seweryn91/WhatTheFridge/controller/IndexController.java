65858a33e1f6cc4ab8edd31008fb90eee0170f8b
Seweryn Talaj <s.talaj@gmail.com>
2023-01-03 17:54:08 +0100

    Add methods to IndexController

--- src/main/java/com/github/Seweryn91/WhatTheFridge/controller/IndexController.java
+++ src/main/java/com/github/Seweryn91/WhatTheFridge/controller/IndexController.java
@@ -1,4 +1,28 @@
 package com.github.Seweryn91.WhatTheFridge.controller;
 
+import com.github.Seweryn91.WhatTheFridge.repository.DishRepository;
+import com.github.Seweryn91.WhatTheFridge.repository.IngredientRepository;
+import com.github.Seweryn91.WhatTheFridge.service.IngredientServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class IndexController {
+
+    @Autowired
+    IngredientRepository ingredientRepository;
+
+    @Autowired
+    private IngredientServiceImpl ingredientService;
+
+    @Autowired
+    DishRepository dishRepository;
+
+    @GetMapping
+    String getIngredients(Model model) {
+        model.addAttribute("ingredients", ingredientService.getAllIngredients());
+        return "ingredients";
+    }
 }
