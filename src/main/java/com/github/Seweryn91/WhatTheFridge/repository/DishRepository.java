3859438e42a7ca3d8a1c538c58df2062c5e062c9
Seweryn Talaj <s.talaj@gmail.com>
2023-05-14 16:42:13 +0200

    Resolve issue with duplicated SQL alias ID

--- src/main/java/com/github/Seweryn91/WhatTheFridge/repository/DishRepository.java
+++ src/main/java/com/github/Seweryn91/WhatTheFridge/repository/DishRepository.java
@@ -11,18 +11,18 @@ import java.util.Set;
 @Repository
 public interface DishRepository extends JpaRepository<Dish, Long> {
 
-    @Query(value = "SELECT * FROM Dish AS d LEFT JOIN dish_ingredient AS di " +
-            "ON d.id = di.fk_dish_id LEFT JOIN ingredient AS i ON di.fk_ingredient_id = i.id " +
+    @Query(value = "SELECT d.id, d.name, d.sweetdish, d.picturelink, d.recipelink FROM Dish AS d LEFT JOIN " +
+            "dish_ingredient AS di ON d.id = di.fk_dish_id LEFT JOIN ingredient AS i ON di.fk_ingredient_id = i.id " +
             "WHERE i.name IN :names ORDER BY d.name ASC", nativeQuery = true)
     Set<Dish> findByIngredientNamesIn(@Param("names") Set<String> ingredientNames);
 
-    @Query(value = "SELECT * FROM Dish AS d LEFT JOIN dish_ingredient AS di " +
-            "ON d.id = di.fk_dish_id LEFT JOIN ingredient AS i ON di.fk_ingredient_id = i.id " +
+    @Query(value = "SELECT d.id, d.name, d.sweetdish, d.picturelink, d.recipelink FROM Dish AS d LEFT JOIN " +
+            "dish_ingredient AS di ON d.id = di.fk_dish_id LEFT JOIN ingredient AS i ON di.fk_ingredient_id = i.id " +
             "WHERE i.name IN :names AND sweetdish = TRUE ORDER BY d.name ASC", nativeQuery = true)
     Set<Dish> findSweetByIngredientNamesIn(@Param("names") Set<String> ingredientNames);
 
-    @Query(value = "SELECT * FROM Dish AS d LEFT JOIN dish_ingredient AS di " +
-            "ON d.id = di.fk_dish_id LEFT JOIN ingredient AS i ON di.fk_ingredient_id = i.id " +
+    @Query(value = "SELECT d.id, d.name, d.sweetdish, d.picturelink, d.recipelink FROM Dish AS d LEFT JOIN " +
+            "dish_ingredient AS di ON d.id = di.fk_dish_id LEFT JOIN ingredient AS i ON di.fk_ingredient_id = i.id " +
             "WHERE i.name IN :names AND sweetdish = FALSE ORDER BY d.name ASC", nativeQuery = true)
     Set<Dish> findSavoryByIngredientNamesIn(@Param("names") Set<String> ingredientNames);
 
