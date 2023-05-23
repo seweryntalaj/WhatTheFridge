75e2efeff8397d65a686c82a92e189a09c6a9ec6
Seweryn Talaj <s.talaj@gmail.com>
2023-05-23 22:11:34 +0200

    Create SecurityConfiguration Class

---
+++ src/main/java/com/github/Seweryn91/WhatTheFridge/security/SecurityConfiguration.java
@@ -0,0 +1,57 @@
+package com.github.Seweryn91.WhatTheFridge.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.Http403ForbiddenEntryPoint;
+
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+
+    public static final String[] ENDPOINT_WHITELIST = {"/", "/index", "/dishes", "dish/get/*", "/allingredients",
+            "/login", "/bootstrap/**"};
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService() {
+        UserDetails admin = User.withUsername("admin")
+                .password(passwordEncoder().encode("admin"))
+                .roles("ADMIN").build();
+        return new InMemoryUserDetailsManager(admin);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+   @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http.authorizeHttpRequests(requests -> requests
+                        .requestMatchers(ENDPOINT_WHITELIST).permitAll()
+                .anyRequest().authenticated())
+                .formLogin(form -> form
+                        .loginPage("/login")
+                        .defaultSuccessUrl("/", true)
+                        .permitAll())
+                .logout(logout -> logout
+                        //.logoutUrl("/logout")
+                        .logoutSuccessUrl("/login?logout")
+                        .invalidateHttpSession(true)
+                        .clearAuthentication(true)
+                        .deleteCookies("JSESSIONID"))
+                .exceptionHandling().authenticationEntryPoint(new Http403ForbiddenEntryPoint());
+
+        return http.build();
+    }
+
+}
