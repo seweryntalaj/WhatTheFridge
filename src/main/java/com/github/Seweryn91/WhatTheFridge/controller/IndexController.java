fbcab16af3b839ec99474870f25cfca210dbd3b0
Seweryn Talaj <s.talaj@gmail.com>
2023-06-15 19:14:37 +0200

    Alter method getIngredients to return sorted list

--- src/main/java/com/github/Seweryn91/WhatTheFridge/controller/IndexController.java
+++ src/main/java/com/github/Seweryn91/WhatTheFridge/controller/IndexController.java
@@ -1,5 +1,6 @@
 package com.github.Seweryn91.WhatTheFridge.controller;
 
+import com.github.Seweryn91.WhatTheFridge.model.Ingredient;
 import com.github.Seweryn91.WhatTheFridge.repository.DishRepository;
 import com.github.Seweryn91.WhatTheFridge.repository.IngredientRepository;
 import com.github.Seweryn91.WhatTheFridge.service.IngredientServiceImpl;
@@ -7,6 +8,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Collections;
+import java.util.List;
 
 @Controller
 public class IndexController {
@@ -22,7 +26,9 @@ public class IndexController {
 
     @GetMapping("/allingredients")
     String getIngredients(Model model) {
-        model.addAttribute("ingredients", ingredientService.getAllIngredients());
+        List<Ingredient> allIngredients = ingredientService.getAllIngredients();
+        Collections.sort(allIngredients);
+        model.addAttribute("ingredients", allIngredients);
         return "allingredients";
     }
 
